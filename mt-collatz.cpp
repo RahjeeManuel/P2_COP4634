115919385575173637d8fc472bd587d3da3fee08
RahjeeManuel <ram88@students.uwf.edu>
2021-10-15 20:11:26 -0500

    comments added

--- mt-collatz.cpp
+++ mt-collatz.cpp
@@ -6,6 +6,8 @@
 #define HISTOGRAM_SIZE 1000
 #define BILLION 1E9
 using namespace std;
+
+//Calculates the collatz # for n
 int collatz(long n) {
     int i = 0;
     while (n != 1) {
@@ -17,14 +19,20 @@ int collatz(long n) {
     }
     return i;
 }
+
 int main(int argc, char* argv[]) {
+    //Program args
     const int N = argc >= 1 && argv[1] ? atoi(argv[1]) : 10000;
     const int T = argc >= 2 && argv[2] ? atoi(argv[2]) : 8;
     const bool NO_LOCK = argc >= 3 && argv[3] ? string(argv[3]) == string("-nolock") : false;
 
+    //Create thread and histogram array
     thread threads[MAX_THREADS];
     int histogram[HISTOGRAM_SIZE] = {};
     
+    //Lambda function to find the collat # for each N
+    //Records each frequency into the histogram array
+    //Uses lock_guard if -nolock parameter exists
     int counter = 2;
     mutex counter_mutex;
     auto record = [&]() {
@@ -44,26 +52,33 @@ int main(int argc, char* argv[]) {
         }
     };
 
+    //Clock variables
     struct timespec start, stop;
     double elapsed_time;
 
+    //Get start time
     clock_gettime(CLOCK_REALTIME, &start);
 
+    //record() is ran for T # of threads
     for (auto i = 0; i < T; i++){
         threads[i] = thread(record);
     }
 
+    //Join all threads when finished
     for (auto i = 0; i < T; i++) {
         if (threads[i].joinable())
             threads[i].join();
     }
 
+    //Get stop time
     clock_gettime(CLOCK_REALTIME, &stop);
     elapsed_time = (stop.tv_sec - start.tv_sec) + (stop.tv_nsec - start.tv_nsec) / BILLION;
 
+    //Print histogram array to cout
     for (auto i = 0; i < HISTOGRAM_SIZE; i++)
         cout << i << "," << histogram[i] << endl;
 
+    //Print elapsed_time to cerr
     cerr << N << "," << T << "," << elapsed_time << endl;
     
     return 0;
